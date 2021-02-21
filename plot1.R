cbc2547ef1477ab0ae13e2c8b1e035ee6cc5520f
RishabhKumar5010 <74526647+RishabhKumar5010@users.noreply.github.com>
2021-02-21 14:29:31 +0530

    update plot1
    Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008. 
    
    >Add unit of emission on y-axis

--- plot1.R
+++ plot1.R
@@ -6,7 +6,7 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 yearemm <- tapply(NEI$Emissions,NEI$year,sum)
 plot(x = names(yearemm),y = yearemm,type="l",col='red',xlab = "years",
-     ylab = "Yealy Emissions",main="Year-wise Emission Trends",
+     ylab = "Yealy Emissions   (tons)",main="Year-wise Emission Trends",
      ylim = c(2e+06,8e+06),xlim = c(1999,2008),xaxt = 'n')
 axis(side = 1, at = seq(1999,2008,3))
 points(x = names(yearemm),y = yearemm,pch = 20,cex = 2)
